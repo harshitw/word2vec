f6f1f639c6bd3d4ad46cc155a03adbf04859fe89
Harshit Awasthi <harshit.awasthi33@gmail.com>
2018-03-26 14:33:28 +0530

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -68,3 +68,102 @@ with train_graph.as_default():
 
     cost = tf.reduce_mean(loss)
     optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+# validation    
+with train_graph.as_default():
+
+    valid_size = 16 # Random set of words to evaluate similarity on.
+    valid_window = 100
+    # pick 8 samples from (0,100) and (1000,1100) each ranges. lower id implies more frequent 
+    valid_examples = np.array(random.sample(range(valid_window), valid_size//2))
+    valid_examples = np.append(valid_examples, 
+                               random.sample(range(1000,1000+valid_window), valid_size//2))
+
+    valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+    
+    # We use the cosine distance:
+    norm = tf.sqrt(tf.reduce_sum(tf.square(embedding), 1, keep_dims=True))
+    normalized_embedding = embedding / norm
+    valid_embedding = tf.nn.embedding_lookup(normalized_embedding, valid_dataset)
+    similarity = tf.matmul(valid_embedding, tf.transpose(normalized_embedding))
+    
+!mkdir checkpoints
+
+
+epochs = 10
+batch_size = 1000
+window_size = 10
+
+with train_graph.as_default():
+    saver = tf.train.Saver()
+
+with tf.Session(graph=train_graph) as sess:
+    iteration = 1
+    loss = 0
+    sess.run(tf.global_variables_initializer())
+
+    for e in range(1, epochs+1):
+        batches = get_batches(train_words, batch_size, window_size)
+        start = time.time()
+        for x, y in batches:
+            
+            feed = {inputs: x,
+                    labels: np.array(y)[:, None]}
+            train_loss, _ = sess.run([cost, optimizer], feed_dict=feed)
+            
+            loss += train_loss
+            
+            if iteration % 100 == 0: 
+                end = time.time()
+                print("Epoch {}/{}".format(e, epochs),
+                      "Iteration: {}".format(iteration),
+                      "Avg. Training loss: {:.4f}".format(loss/100),
+                      "{:.4f} sec/batch".format((end-start)/100))
+                loss = 0
+                start = time.time()
+            
+            if iteration % 1000 == 0:
+                # note that this is expensive (~20% slowdown if computed every 500 steps)
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = int_to_vocab[valid_examples[i]]
+                    top_k = 8 # number of nearest neighbors
+                    nearest = (-sim[i, :]).argsort()[1:top_k+1]
+                    log = 'Nearest to %s:' % valid_word
+                    for k in range(top_k):
+                        close_word = int_to_vocab[nearest[k]]
+                        log = '%s %s,' % (log, close_word)
+                    print(log)
+            
+            iteration += 1
+    save_path = saver.save(sess, "checkpoints/text8.ckpt")
+    embed_mat = sess.run(normalized_embedding)
+
+# restoring the trained network 
+with train_graph.as_default():
+    saver = tf.train.Saver()
+
+with tf.Session(graph=train_graph) as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('checkpoints'))
+    embed_mat = sess.run(embedding)
+
+# visualizing the word vectors    
+%matplotlib inline
+%config InlineBackend.figure_format = 'retina'
+
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+
+viz_words = 500
+tsne = TSNE()
+embed_tsne = tsne.fit_transform(embed_mat[:viz_words, :])
+
+fig, ax = plt.subplots(figsize=(14, 14))
+for idx in range(viz_words):
+    plt.scatter(*embed_tsne[idx, :], color='steelblue')
+    plt.annotate(int_to_vocab[idx], (embed_tsne[idx, 0], embed_tsne[idx, 1]), alpha=0.7)
+
+    
+
+    
